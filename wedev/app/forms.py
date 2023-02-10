0373aff2e78f759da93f533155695db34df91838
Victor Henrique Silva <henrictor@hotmail.com>
2023-02-10 19:25:29 -0300

    feat: added submit button to register staff route

--- wedev/app/forms.py
+++ wedev/app/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class RegisterStaffForm(forms.Form):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('submit', 'Submit'))
     
     name = forms.CharField()
     lastname = forms.CharField()
@@ -8,4 +15,5 @@ class RegisterStaffForm(forms.Form):
     state = forms.CharField()
     city = forms.CharField()
     address = forms.CharField()
-    phone_number = forms.IntegerField()
+    phone_number = forms.CharField()
+    
--- wedev/app/models.py
+++ wedev/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class RegisterStaff(models.Model):
+    name = models.CharField(max_length=55, editable=True, blank=False, null=False)
+    lastname = models.CharField(max_length=55, editable=True, blank=False, null=False)
+    email = models.EmailField(max_length=70, editable=True, blank=False, null=False)
+    state = models.CharField(max_length=2, editable=True, blank=False, null=False)
+    city = models.CharField(max_length=30, editable=True, blank=False, null=False)
+    address = models.CharField(max_length=70, editable=True, blank=False, null=False)
+    phone_number = models.CharField(max_length=11)
