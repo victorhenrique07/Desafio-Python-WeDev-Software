5d73a4b83e6018a7cec46b0924bdced9201e2414
Victor Henrique Silva <henrictor@hotmail.com>
2023-02-13 16:03:25 -0300

    feat: added accounts/ route to login/logout management

--- wedev/app/admin.py
+++ wedev/app/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from app.models import Staff
+from app.models import Usuario, Telefone, Curso, Aluno, AssociacaoAlunoUsuario
+
+
+class TelefoneAdmin(admin.TabularInline):
+    model = Telefone
+
+
+class UsuarioAdmin(admin.ModelAdmin):
+    
+    inlines = [TelefoneAdmin,]
+    
 
 # Register your models here.
-admin.site.register(Staff)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Telefone)
+admin.site.register(Curso)
+admin.site.register(Aluno)
+admin.site.register(AssociacaoAlunoUsuario)
