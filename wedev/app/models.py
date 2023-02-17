4965b8e73f54b9a1f04a23678b9266e870768707
Victor Henrique Silva <henrictor@hotmail.com>
2023-02-17 19:27:43 -0300

    feat(models): changing models schemas

--- wedev/app/models.py
+++ wedev/app/models.py
@@ -1,35 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
 
 # Create your models here.
-class Pessoa(models.Model):
-    name = models.CharField(max_length=55, editable=True, blank=False, null=False)
-    lastname = models.CharField(max_length=55, editable=True, blank=False, null=False)
-    email = models.EmailField(max_length=70, editable=True, blank=False, null=False)
-    state = models.CharField(max_length=2, editable=True, blank=False, null=False)
-    city = models.CharField(max_length=30, editable=True, blank=False, null=False)
-    address = models.CharField(max_length=70, editable=True, blank=False, null=False)
-    username = models.CharField(max_length=20, editable=True, blank=False, null=False)
-    password = models.CharField(max_length=26, editable=True, blank=False, null=False)
+class CustomUser(AbstractUser):
+    TIPO_PESSOA = [
+        ('aluno', 'Aluno'), 
+        ('professor', 'Professor')
+    ]    
     
-class Aluno(Pessoa):
-    pass
+    type = models.CharField(max_length=9, editable=True, blank=False, null=False, choices=TIPO_PESSOA)
+    state = models.CharField(max_length=2, editable=True, blank=True, null=False)
+    city = models.CharField(max_length=30, editable=True, blank=True, null=False)
+    address = models.CharField(max_length=70, editable=True, blank=True, null=False)
 
-class Professor(Pessoa):
-    pass
-
-
-class Telefone(models.Model):
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, related_name='TelefoneProfessor', null=True)
-    telefone = models.CharField(max_length=255)
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        
+    def __str__(self):
+        return "%s %s %s" % (self.state, self.city, self.address)
     
 class Curso(models.Model):
     active = models.BooleanField()
     name = models.CharField(max_length=80, editable=True, blank=False, null=True)
     start_date = models.DateField()
     finish_date = models.DateField()
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, verbose_name="professor do curso")
-#
-class EntidadeAssociativa(models.Model):
-    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s %s %s %s %s" % (
+            self.active,
+            self.name,
+            self.start_date,
+            self.finish_date,
+        )
+
+
+class Infos(models.Model):
+    name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    telefone = models.CharField(max_length=255)
