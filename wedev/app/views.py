b2e4cd2252a4956d5d45a3ce214e73fbf46faa89
Victor Henrique Silva <henrictor@hotmail.com>
2023-02-19 05:56:59 -0300

    feat(user): adding route to update user informations.

--- wedev/app/views.py
+++ wedev/app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from .forms import CursoForm, CustomUserChangeForm, CustomUserPainel
+from django.contrib.auth.models import User
+from .models import CustomUser
+from django.contrib import messages
 
 # Create your views here.
 
-def register(request):
-    return "hi"
+@login_required(login_url='login')
+def home(request):
+    if request.method == 'POST':
+       form = CustomUserChangeForm(request.POST, instance=request.user)
+       if form.is_valid():
+           form.save()
+           messages.success(request, "Deu certo")
+           return redirect('/app')
+    form = CustomUserChangeForm()
+    context = {
+        'form': form,
+        'user': request.user
+    }
+    return render(request, "index.html", context)
+
+@login_required
+def student_course(request):
+    return render(request, "index.html", {'form': CustomUserPainel()})
