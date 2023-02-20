638c3bcfec249974c601803e4dcb4122df1c48df
Victor Henrique Silva <henrictor@hotmail.com>
2023-02-19 22:40:09 -0300

    feat(updateuser): adding update user function

--- wedev/app/views.py
+++ wedev/app/views.py
@@ -1,27 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import CursoForm, CustomUserChangeForm, CustomUserPainel
+from .forms import CursoForm, AlunoForm, ProfessorForm
 from django.contrib.auth.models import User
-from .models import CustomUser
+from .models import Telefone, Aluno, Professor
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 @login_required(login_url='login')
-def home(request):
+@csrf_exempt
+def updateuser(request, pk):
+    user_id = Aluno.objects.get(id=pk)
+    form = AlunoForm(instance=teste)
     if request.method == 'POST':
-       form = CustomUserChangeForm(request.POST, instance=request.user)
-       if form.is_valid():
-           form.save()
-           messages.success(request, "Deu certo")
-           return redirect('/app')
-    form = CustomUserChangeForm()
+        form = AlunoForm(request.POST, instance=user_id)
+        if form.is_valid():
+            form.save()
+            print('deu certo')
+            return redirect('/accounts/login')
+        
+    form = AlunoForm()
     context = {
-        'form': form,
-        'user': request.user
-    }
+        'form': form
+    }    
     return render(request, "index.html", context)
+
+@login_required(login_url='login')
+def teste(request):
+    pessoa = request.user
+    telefone = Telefone.objects.get(id=2)
+    if telefone is None:
+        print('Usuario não tem telefone.')
+    print(pessoa.id)
+    context = {
+        'form': pessoa
+    }
+    return render(request, 'cursos_usuario.html', context)
 
 @login_required
 def student_course(request):
-    return render(request, "index.html", {'form': CustomUserPainel()})
+    return render(request, "index.html", {'form': AlunoForm()})
